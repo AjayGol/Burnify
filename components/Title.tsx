28471abf961eb24c13a4e5e401f03c79fa50542c
Ajay Gol <ajay@infinisync.io>
2024-05-09 17:38:16 +0530

    Update title

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,14 +1,25 @@
-import type { PropsWithChildren, ReactElement } from 'react';
-import {StyleSheet, Text, useColorScheme} from 'react-native';
+import type { PropsWithChildren } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+
 type Props = PropsWithChildren<{
-  text: string;
+    text: string;
+    icon?: string | null;
 }>;
 
-export default function TitleView({
-                                    text,
-                                           }: Props) {
-
-  return (
-      <Text style={{fontWeight:'600', fontSize: 26}}>{text}</Text>
-  );
+export default function TitleView({ text, icon }: Props) {
+    return (
+        <View style={styles.mainView}>
+            <Text style={{ fontWeight: '600', fontSize: 26 }}>{text}</Text>
+            {(icon && <FontAwesome5 name={icon} size={18} color="black" />) || null}
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    mainView: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+    },
+});
